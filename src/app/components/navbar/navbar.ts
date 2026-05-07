38c87484006fbe47ab146758b7cbf52499d289de
Lucas Marinho <ls.marinho36@gmail.com>
2026-05-07 08:02:02 -0300

    adicionando side bar retrátil

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+
+  aberto = true;
+
   constructor(private router: Router) {}
+
+  toggleSidebar() {
+    this.aberto = !this.aberto;
+  }
 
   logout() {
     localStorage.removeItem('token');
